f04e9b395fffa4d0550c95af71329c43b1eabc1e
season1618 <seasonminority314@gmail.com>
2021-09-16 18:24:13 +0900

    rewrite the basic function as function, fix eq

--- lisp.c
+++ lisp.c
@@ -6,11 +6,50 @@ FILE* fp;
 
 long *t, *nil;
 
+long* atom(long* x);
+long* eq(long* x, long* y);
+long* car(long* x);
+long* cdr(long* y);
+long* cons(long* x, long* y);
+
 bool equal(char* a, char* b, int n);
 long* number(long i);
 char* function();
 long* parentheses();
 long* expression();
+
+long* atom(long* x){
+    if(*(x + 1) == *nil) return t;
+    else return nil;
+}
+long* eq(long* x, long* y){
+    if(atom(x) == t){
+        if(atom(y) == t){
+            if(*x == *y) return t;
+            else return nil;
+        }else return nil;
+    }else{
+        if(atom(y) == t) return nil;
+        else{
+            if(eq((long*)*x, (long*)*y) == t && eq((long*)*(x + 1), (long*)*(y + 1)) == t) return t;
+            else return nil;
+        }
+    }
+}
+long* car(long* x){
+    if(atom(x) == t) return nil;
+    else return (long*)*x;
+}
+long* cdr(long* x){
+    if(atom(x) == t) return nil;
+    else return (long*)*(x + 1);
+}
+long* cons(long* x, long* y){
+    long* p = calloc(2, sizeof(long));
+    *p = (long)x;
+    *(p + 1) = (long)y;
+    return p;
+}
 
 bool equal(char* a, char* b, int n){
     for(int i = 0; i < n; i++){
@@ -49,27 +88,22 @@ long* parentheses(){
     if(equal(func, "eq", 2)){
         long* a = expression();
         long* b = expression();
-        if(*a == *b && *(a + 1) == *(b + 1)) return (long*)t;
-        else return (long*)nil;
+        return eq(a, b);
     }
     else if(equal(func, "car", 3)){
         long* p = expression();
-        return (long*)*p;
+        return car(p);
     }else if(equal(func, "cdr", 3)){
         long* p = expression();
-        return (long*)*(p + 1);
+        return cdr(p);
     }
     else if(equal(func, "atom", 4)){
         long* p = expression();
-        if(*(p + 1) == *nil) return (long*)t;
-        else return (long*)nil;
+        return atom(p);
     }else if(equal(func, "cons", 4)){
         long* a = expression();
         long* b = expression();
-        long* p = calloc(2, sizeof(long));
-        *p = (long)a;
-        *(p + 1) = (long)b;
-        return p;
+        return cons(a, b);
     }
 }
 long* expression(){
